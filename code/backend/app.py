8d022cfc79fdf5f1c44c96fceb0c088c18022ceb
VishvaNavanjana <vishvanavanjana@gmail.com>
2023-10-16 22:14:45 +0530

    add files

--- code/backend/app.py
+++ code/backend/app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/health')
+@app.route('/health', methods=['GET'])
 def health():
     return "Ok"
+
+@app.route('/api', methods=['POST'])
+def api():
+    try:
+        # Extract JSON data from request
+        request_data = request.get_json()
+        question = request_data.get('question')
+        
+        response_data = {"response": f"You asked: {question}"}
+
+        return jsonify(response_data), 200
+        
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
